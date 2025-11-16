bcf4544ae232d505d2adbc534cbd6994e1cd0174
Bibeduck <bibev1322@gmail.com>
2025-11-16 14:29:45 +0800

    data types revision

--- scripts/sql_scripts/create_staging_tables.sql
+++ scripts/sql_scripts/create_staging_tables.sql
@@ -5,7 +5,7 @@
 --Product
 DROP TABLE IF EXISTS staging.stg_product_list;
 CREATE TABLE staging.stg_product_list(
-	product_id INT,
+	product_id VARCHAR(25),
 	product_name VARCHAR(50),
 	product_type VARCHAR(50),
 	price DECIMAL(8, 2),
@@ -20,7 +20,7 @@ DROP TABLE IF EXISTS staging.stg_line_items_products;
 CREATE TABLE staging.stg_line_items_products(
 	order_id VARCHAR(50),
 	product_name VARCHAR(100),
-	product_id INT,
+	product_id VARCHAR(25),
 	-- additional metadata for data lineage/tracking
 	source_filename VARCHAR (50),
 	ingestion_data TIMESTAMP
@@ -30,7 +30,7 @@ DROP TABLE IF EXISTS staging.stg_line_items_prices;
 CREATE TABLE staging.stg_line_items_prices(
 	order_id VARCHAR(50),
 	price DECIMAL(8, 2),
-	quantity INT,
+	quantity VARCHAR(10),
 	-- additional metadata for data lineage/tracking
 	source_filename VARCHAR (50),
 	ingestion_data TIMESTAMP
@@ -41,8 +41,8 @@ CREATE TABLE staging.stg_line_items_prices(
 DROP TABLE IF EXISTS staging.stg_orders;
 CREATE TABLE staging.stg_orders(
 	order_id VARCHAR(50),
-	user_id INT,
-	estimated_arrival INT,
+	user_id VARCHAR(25),
+	estimated_arrival VARCHAR(10),
 	transaction_data DATE,
 	-- additional metadata for data lineage/tracking
 	source_filename VARCHAR (50),
@@ -62,11 +62,11 @@ CREATE TABLE staging.stg_order_delays(
 --Customer
 DROP TABLE IF EXISTS stagin.stg_user_data;
 CREATE TABLE staging.stg_user_data(
-	user_id INT,
+	user_id VARCHAR(25),
 	creation_date TIMESTAMP,
 	name VARCHAR(50),
 	street VARCHAR(50),
-	state VARCHAR(25),
+	state VARCHAR(50),
 	city VARCHAR(25),
 	country VARCHAR(60),
 	birthdate TIMESTAMP,
@@ -80,7 +80,7 @@ CREATE TABLE staging.stg_user_data(
 
 DROP TABLE IF EXISTS ng.stg_user_job;
 CREATE TABLE staging.stg_user_job(
-	user_id INT,
+	user_id VARCHAR(25),
 	name VARCHAR(50),
 	job_title VARCHAR(25),
 	job_level VARCHAR(25),
@@ -91,10 +91,10 @@ CREATE TABLE staging.stg_user_job(
 
 DROP TABLE IF EXISTS staging.stg_user_credit_card;
 CREATE TABLE staging.stg_user_credit_card(
-	user_id INT,
+	user_id VARCHAR(25),
 	name VARCHAR(50),
-	credit_card_number VARCHAR(10),
-	issuing_bank VARCHAR(50),
+	credit_card_number INT,
+	issuing_bank VARCHAR(25),
 	-- additional metadata for data lineage/tracking
 	source_filename VARCHAR (50),
 	ingestion_data TIMESTAMP
@@ -103,7 +103,7 @@ CREATE TABLE staging.stg_user_credit_card(
 --Enterprise/Merchant
 DROP TABLE IF EXISTS staging.stg_merchant_data;
 CREATE TABLE staging.stg_merchant_data(
-	merchant_id INT,
+	merchant_id VARCHAR(25),
 	creation_date DATE,
 	name VARCHAR(50),
 	street VARCHAR(50),
@@ -111,8 +111,6 @@ CREATE TABLE staging.stg_merchant_data(
 	city VARCHAR (25),
 	country VARCHAR(60),
 	contact_number VARCHAR(50),
-	
-	
 	-- additional metadata for data lineage/tracking
 	source_filename VARCHAR (50),
 	ingestion_data TIMESTAMP
@@ -121,8 +119,8 @@ CREATE TABLE staging.stg_merchant_data(
 DROP TABLE IF EXISTS staging.stg_order_merchant;
 CREATE TABLE staging.stg_order_merchant(
 	order_id VARCHAR(50),
-	merchant_id INT,
-	staff_id VARCHAR(10),
+	merchant_id VARCHAR(25),
+	staff_id VARCHAR(25),
 	-- additional metadata for data lineage/tracking
 	source_filename VARCHAR (50),
 	ingestion_data TIMESTAMP
@@ -131,7 +129,7 @@ CREATE TABLE staging.stg_order_merchant(
 --Staff
 DROP TABLE IF EXISTS staging.stg_staff;
 CREATE TABLE staging.stg_staff(
-	staff_id VARCHAR(10),
+	staff_id VARCHAR(25),
 	name VARCHAR(50),
 	job_level VARCHAR(25),
 	street VARCHAR(50),
@@ -148,10 +146,10 @@ CREATE TABLE staging.stg_staff(
 --Marketing
 DROP TABLE IF EXISTS staging.stg_campaign;
 CREATE TABLE staging.stg_campaign(
-	campaign_id VARCHAR(10),
+	campaign_id VARCHAR(25),
 	campaign_name VARCHAR(25),
 	campaign_description VARCHAR(50),
-	discount DECIMAL(4, 2),
+	discount VARCHAR(10),
 	-- additional metadata for data lineage/tracking
 	source_filename VARCHAR (50),
 	ingestion_data TIMESTAMP
@@ -160,7 +158,7 @@ CREATE TABLE staging.stg_campaign(
 DROP TABLE IF EXISTS staging.stg_campaign_transactions;
 CREATE TABLE staging.stg_campaign_transactions(
 	transaction_date DATE,
-	campaign_id VARCHAR(10),
+	campaign_id VARCHAR(25),
 	order_id VARCHAR(50),
 	estimated_arrival INT,
 	availed INT,
