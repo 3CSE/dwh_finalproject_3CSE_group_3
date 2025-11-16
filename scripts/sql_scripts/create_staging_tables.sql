2c59f85bedc320718e34cec02712564c8c446e7b
Bibeduck <bibev1322@gmail.com>
2025-11-16 15:27:58 +0800

    change data type from varchar to text

--- scripts/sql_scripts/create_staging_tables.sql
+++ scripts/sql_scripts/create_staging_tables.sql
@@ -8,12 +8,12 @@ CREATE SCHEMA IF NOT EXISTS staging;
 --Product
 DROP TABLE IF EXISTS staging.stg_product_list;
 CREATE TABLE staging.stg_product_list(
-	product_id VARCHAR(25),
-	product_name VARCHAR(50),
-	product_type VARCHAR(50),
+	product_id TEXT,
+	product_name TEXT,
+	product_type TEXT,
 	price DECIMAL(8, 2),
 	-- additional metadata for data lineage/tracking
-	source_filename VARCHAR (50),
+	source_filename TEXT,
 	ingestion_date TIMESTAMP
 );
 
@@ -21,21 +21,21 @@ CREATE TABLE staging.stg_product_list(
 --Line Items
 DROP TABLE IF EXISTS staging.stg_line_items_products;
 CREATE TABLE staging.stg_line_items_products(
-	order_id VARCHAR(50),
-	product_name VARCHAR(100),
-	product_id VARCHAR(25),
+	order_id TEXT,
+	product_name TEXT,
+	product_id TEXT,
 	-- additional metadata for data lineage/tracking
-	source_filename VARCHAR (50),
+	source_filename TEXT,
 	ingestion_date TIMESTAMP
 );
 
 DROP TABLE IF EXISTS staging.stg_line_items_prices;
 CREATE TABLE staging.stg_line_items_prices(
-	order_id VARCHAR(50),
+	order_id TEXT,
 	price DECIMAL(8, 2),
-	quantity VARCHAR(10),
+	quantity TEXT,
 	-- additional metadata for data lineage/tracking
-	source_filename VARCHAR (50),
+	source_filename TEXT,
 	ingestion_date TIMESTAMP
 );
 
@@ -43,50 +43,50 @@ CREATE TABLE staging.stg_line_items_prices(
 
 DROP TABLE IF EXISTS staging.stg_orders;
 CREATE TABLE staging.stg_orders(
-	order_id VARCHAR(50),
-	user_id VARCHAR(25),
-	estimated_arrival VARCHAR(10),
+	order_id TEXT,
+	user_id TEXT,
+	estimated_arrival TEXT,
 	transaction_data DATE,
 	-- additional metadata for data lineage/tracking
-	source_filename VARCHAR (50),
+	source_filename TEXT,
 	ingestion_date TIMESTAMP
 );
 
 --Order Delays
 DROP TABLE IF EXISTS staging.stg_order_delays;
 CREATE TABLE staging.stg_order_delays(
-	order_id VARCHAR(50),
+	order_id TEXT,
 	delay_in_days INT,
 	-- additional metadata for data lineage/tracking
-	source_filename VARCHAR (50),
+	source_filename TEXT,
 	ingestion_date TIMESTAMP
 );
 
 --Customer
 DROP TABLE IF EXISTS staging.stg_user_data;
 CREATE TABLE staging.stg_user_data(
-	user_id VARCHAR(25),
+	user_id TEXT,
 	creation_date TIMESTAMP,
-	name VARCHAR(50),
-	street VARCHAR(50),
-	state VARCHAR(50),
-	city VARCHAR(25),
-	country VARCHAR(60),
+	name TEXT,
+	street TEXT,
+	state TEXT,
+	city TEXT,
+	country TEXT,
 	birthdate TIMESTAMP,
-	gender VARCHAR(10),
-	device_address VARCHAR(25),
-	user_type VARCHAR(10),
+	gender TEXT,
+	device_address TEXT,
+	user_type TEXT,
 	-- additional metadata for data lineage/tracking
-	source_filename VARCHAR (50),
+	source_filename TEXT,
 	ingestion_date TIMESTAMP
 );
 
 DROP TABLE IF EXISTS staging.stg_user_job;
 CREATE TABLE staging.stg_user_job(
-	user_id VARCHAR(25),
-	name VARCHAR(50),
-	job_title VARCHAR(25),
-	job_level VARCHAR(25),
+	user_id TEXT,
+	name TEXT,
+	job_title TEXT,
+	job_level TEXT,
 	-- additional metadata for data lineage/tracking
 	source_filename VARCHAR (50),
 	ingestion_date TIMESTAMP
@@ -94,78 +94,78 @@ CREATE TABLE staging.stg_user_job(
 
 DROP TABLE IF EXISTS staging.stg_user_credit_card;
 CREATE TABLE staging.stg_user_credit_card(
-	user_id VARCHAR(25),
-	name VARCHAR(50),
+	user_id TEXT,
+	name TEXT,
 	credit_card_number INT,
-	issuing_bank VARCHAR(25),
+	issuing_bank TEXT,
 	-- additional metadata for data lineage/tracking
-	source_filename VARCHAR (50),
+	source_filename TEXT,
 	ingestion_date TIMESTAMP
 );
 
 --Enterprise/Merchant
 DROP TABLE IF EXISTS staging.stg_merchant_data;
 CREATE TABLE staging.stg_merchant_data(
-	merchant_id VARCHAR(25),
+	merchant_id TEXT,
 	creation_date DATE,
-	name VARCHAR(50),
-	street VARCHAR(50),
-	state VARCHAR(25),
-	city VARCHAR (25),
-	country VARCHAR(60),
-	contact_number VARCHAR(50),
+	name TEXT,
+	street TEXT,
+	state TEXT,
+	city TEXT,
+	country TEXT,
+	contact_number TEXT,
 	-- additional metadata for data lineage/tracking
-	source_filename VARCHAR (50),
+	source_filename TEXT,
 	ingestion_date TIMESTAMP
 );
 
 DROP TABLE IF EXISTS staging.stg_order_merchant;
 CREATE TABLE staging.stg_order_merchant(
-	order_id VARCHAR(50),
-	merchant_id VARCHAR(25),
-	staff_id VARCHAR(25),
+	order_id TEXT,
+	merchant_id TEXT,
+	staff_id TEXT,
 	-- additional metadata for data lineage/tracking
-	source_filename VARCHAR (50),
+	source_filename TEXT,
 	ingestion_date TIMESTAMP
 );
 
 --Staff
 DROP TABLE IF EXISTS staging.stg_staff;
 CREATE TABLE staging.stg_staff(
-	staff_id VARCHAR(25),
-	name VARCHAR(50),
-	job_level VARCHAR(25),
-	street VARCHAR(50),
-	state VARCHAR(25),
-	city VARCHAR(25),
-	country VARCHAR(60),
-	contact_number VARCHAR(25),
+	staff_id TEXT,
+	name TEXT,
+	job_level TEXT,
+	street TEXT,
+	state TEXT,
+	city TEXT,
+	country VTEXT,
+	contact_number TEXT,
 	creation_date TIMESTAMP,
 	-- additional metadata for data lineage/tracking
-	source_filename VARCHAR (50),
+	source_filename TEXT,
 	ingestion_date TIMESTAMP
 );
 
 --Marketing
 DROP TABLE IF EXISTS staging.stg_campaign;
 CREATE TABLE staging.stg_campaign(
-	campaign_id VARCHAR(25),
-	campaign_name VARCHAR(25),
-	campaign_description VARCHAR(50),
-	discount VARCHAR(10),
+	campaign_id TEXT,
+	campaign_name TEXT,
+	campaign_description TEXT,
+	discount TEXT,
 	-- additional metadata for data lineage/tracking
-	source_filename VARCHAR (50),
+	source_filename TEXT,
 	ingestion_date TIMESTAMP
 );
 
 DROP TABLE IF EXISTS staging.stg_campaign_transactions;
 CREATE TABLE staging.stg_campaign_transactions(
 	transaction_date DATE,
-	campaign_id VARCHAR(25),
-	order_id VARCHAR(50),
+	campaign_id TEXT,
+	order_id TEXT,
 	estimated_arrival INT,
 	availed INT,
 	-- additional metadata for data lineage/tracking
-	source_filename VARCHAR (50),
+	source_filename TEXT,
 	ingestion_date TIMESTAMP
 );
