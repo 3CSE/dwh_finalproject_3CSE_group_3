022587e7720e5e5c28893aa4fcd486cf67b4fc32
Bibeduck <bibev1322@gmail.com>
2025-12-05 21:46:54 +0800

    added duplicate flag in clean_user_job

--- sql/clean/clean_user_job.sql
+++ sql/clean/clean_user_job.sql
@@ -10,18 +10,17 @@ WITH source_data AS (
         ingestion_date
     FROM staging.stg_user_job
 ),
+
 cleaned AS (
     SELECT
         TRIM(user_id) AS user_id,
         COALESCE(INITCAP(TRIM(name)), 'Unknown') AS name,
-
         -- Clean job_title
         CASE 
             WHEN job_title ILIKE 'nan' OR job_title IS NULL OR TRIM(job_title) = '' 
                 THEN 'Unknown'
             ELSE INITCAP(TRIM(job_title))
         END AS job_title,
-
         -- Clean job_level
         CASE 
             WHEN job_level ILIKE 'nan' OR job_level IS NULL OR TRIM(job_level) = '' 
@@ -34,11 +33,26 @@ cleaned AS (
     FROM source_data
     WHERE user_id IS NOT NULL AND TRIM(user_id) != ''
 ),
+-- Remove exact duplicates
+cleaned_dedup AS (
+    SELECT *
+    FROM (
+        SELECT
+            *,
+            ROW_NUMBER() OVER (
+                PARTITION BY user_id, name, job_title, job_level, source_filename
+                ORDER BY ingestion_date
+            ) AS exact_dup_rank
+        FROM cleaned
+    ) t
+    WHERE exact_dup_rank = 1
+),
+-- Flag duplicates based on user_id
 dup_flag AS (
     SELECT
         *,
         COUNT(*) OVER (PARTITION BY user_id) AS dup_count
-    FROM cleaned
+    FROM cleaned_dedup
 )
 SELECT
     user_id,
@@ -47,16 +61,14 @@ SELECT
     job_level,
     source_filename,
     ingestion_date,
-
-    CASE 
-        WHEN dup_count > 1 THEN TRUE
-        ELSE FALSE
-    END AS is_duplicate
- 
+    (dup_count > 1) AS is_duplicate
 FROM dup_flag;
 
-
 -- Test the view
+-- Check the count
+-- SELECT COUNT(*) FROM staging.stg_user_job;
+-- SELECT COUNT(*) FROM staging.clean_stg_user_job;
+
+-- Check the cleaned data
 -- SELECT * FROM staging.stg_user_job LIMIT 50;
-SELECT * FROM staging.clean_stg_user_job 
-WHERE user_id = 'USER00304'
+-- SELECT * FROM staging.clean_stg_user_job LIMIT 50;
