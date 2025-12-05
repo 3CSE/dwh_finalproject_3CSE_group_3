6d2118bb772ca851bc3cfc7264178a2489b43345
Bibeduck <bibev1322@gmail.com>
2025-12-05 22:24:43 +0800

    added duplicate flag and made minor revisions in clean_order_delays

--- sql/clean/clean_order_delays.sql
+++ sql/clean/clean_order_delays.sql
@@ -8,6 +8,7 @@ WITH source_data AS (
         ingestion_date
     FROM staging.stg_order_delays
 ),
+
 cleaned AS (
     SELECT
         TRIM(order_id) AS order_id,
@@ -15,26 +16,45 @@ cleaned AS (
         source_filename,
         ingestion_date
     FROM source_data
-    WHERE order_id IS NOT NULL AND TRIM(order_id) != ''
-      AND delay_in_days IS NOT NULL
+    WHERE order_id IS NOT NULL 
+        AND TRIM(order_id) != ''
+        AND delay_in_days IS NOT NULL
 ),
-ranked AS (
+
+dedup_exact AS (
+    SELECT *
+    FROM (
+        SELECT
+            *,
+            ROW_NUMBER() OVER (
+                PARTITION BY order_id, delay_in_days, source_filename
+                ORDER BY ingestion_date DESC
+            ) AS exact_dup_rank
+        FROM cleaned
+    ) t
+    WHERE exact_dup_rank = 1
+),
+
+dup_flag AS (
     SELECT
         *,
-        ROW_NUMBER() OVER (
-            PARTITION BY order_id, delay_in_days, source_filename
-            ORDER BY ingestion_date DESC
-        ) AS rn
-    FROM cleaned
+        COUNT(*) OVER (PARTITION BY order_id) AS dup_count
+    FROM dedup_exact
 )
+
 SELECT
     order_id,
     delay_in_days,
     source_filename,
-    ingestion_date
-FROM ranked
-WHERE rn = 1;
+    ingestion_date,
+    (dup_count > 1) AS is_duplicate
+FROM dup_flag;
 
 -- Test the view
--- SELECT * FROM staging.stg_order_delays LIMIT 10;
--- SELECT * FROM staging.clean_stg_order_delays LIMIT 10;
+-- Check count
+-- SELECT COUNT(*) FROM staging.stg_order_delays;
+-- SELECT COUNT(*) FROM staging.clean_stg_order_delays;
+
+-- Check the cleaned data
+-- SELECT * FROM staging.clean_stg_order_delays LIMIT 50;
+-- SELECT * FROM staging.stg_order_delays LIMIT 50;
